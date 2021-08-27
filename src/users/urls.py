698232ce1beff925438984a506852d65dc336483
500ping <ndt512x@gmail.com>
2021-08-27 16:47:24 +0700

    change to jwt

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from rest_framework.authtoken import views
+# from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path("login/", views.obtain_auth_token, name='obtain-auth-token'),
+    # path("login/", views.obtain_auth_token, name='obtain-auth-token'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] 
